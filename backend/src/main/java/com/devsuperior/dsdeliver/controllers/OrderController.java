ce6cc30e5bb765ce2bf92371f5c0bb18bdba19b4
Anderson Lima <andersonlp2328@gmail.com>
2021-01-10 20:35:42 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,30 +1,40 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
 
-
-@RestController //Controlador reset
-@RequestMapping (value = "/orders")//Caminho do recurso
+@RestController // Controlador reset
+@RequestMapping(value = "/orders") // Caminho do recurso
 public class OrderController {
-	
-	@Autowired //Dependencia para o service
+
+	@Autowired // Dependencia para o service
 	private OrderService service;
-	
-	@GetMapping 
-	public ResponseEntity<List<OrderDTO>> findall(){
+
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findall() {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-		
-	}
-	
 
+	}
+
+	// Salvar
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
+	}
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,13 +1,20 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 //para ser um componente registrado no Spring
 @Service
@@ -15,11 +22,30 @@ public class OrderService {
 
 	@Autowired // Server para injentar as dependencias
 	private OrderRepository repository;
-	
+
+	@Autowired // Server para injentar as dependencias
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+	// Metodo para alterar o banco
+	// Inserir um novo produto já com os pedidos
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 
 	}
 }
